154681d11a95f246c0bbbdcfb2d4bae3a92d9516
Niko <43053729+NovaN4@users.noreply.github.com>
2022-11-27 19:47:32 -0800

    Misc. changes

---
+++ src/main/java/com/nekotune/scorch/common/blocks/BrittleBlocks.java
@@ -0,0 +1,9 @@
+package com.nekotune.scorch.common.blocks;
+
+import net.minecraft.world.level.block.Block;
+
+public class BrittleBlocks extends Block {
+    public BrittleBlocks(Properties properties) {
+        super(properties);
+    }
+}
--- src/main/java/com/nekotune/scorch/common/registry/ModCreativeTabs.java
+++ src/main/java/com/nekotune/scorch/common/registry/ModCreativeTabs.java
@@ -7,7 +7,7 @@ public class ModCreativeTabs {
     public static final CreativeModeTab MOD_TAB = new CreativeModeTab("modtab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.PLANT_FIBER.get());
+            return new ItemStack(ModBlocks.CORRODED_STONE.get());
         }
     };
 }
