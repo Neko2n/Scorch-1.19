f95953d2899d417aaacef59d869aa48bcd4bf146
Niko <43053729+NovaN4@users.noreply.github.com>
2022-12-14 00:01:40 -0800

    Changed creative tab icon to Water Pouch

--- src/main/java/com/nekotune/scorch/common/registry/ModCreativeTabs.java
+++ src/main/java/com/nekotune/scorch/common/registry/ModCreativeTabs.java
@@ -7,7 +7,7 @@ public class ModCreativeTabs {
     public static final CreativeModeTab MOD_TAB = new CreativeModeTab("modtab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModBlocks.CORRODED_STONE.get());
+            return new ItemStack(ModItems.WATER_POUCH.get());
         }
     };
 }
